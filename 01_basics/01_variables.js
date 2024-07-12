bd97214c6877c2da414a4376d3bf83b5dcc44045
Ashi2311 <princekumarashish@gmail.com>
2024-07-12 08:44:04 +0000

    variable and const are done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345 
+let accountEmail = "ashish@gmail.com"
+var accountPass = "12344321" 
+accountCity = "Nawada"
+let accountState ;
+
+// accountId = 23   // not alowed 2nd time when const is available
+
+accountEmail = "prince@gmail.com"
+accountPass = "2345123"
+accountCity = "Nalanda"
+
+
+console.log(accountId)
+console.table([accountId , accountEmail, accountPass , accountCity, accountState]);
