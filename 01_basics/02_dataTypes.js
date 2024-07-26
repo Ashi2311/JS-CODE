8fad62e983e432504d7e21dc91433122656cf153
Ashi2311 <princekumarashish@gmail.com>
2024-07-26 15:53:11 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js to new version
+
+// alert(3+3)  // we are using JAVA SCRIPT not browser 
+
+// DATATYPES
+
+let name = "Ashish"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint (not in use normally)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique  
+
+// object
+
+
+console.log(typeof "Ashish");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
